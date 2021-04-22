a9f30571215c964dd31ad2e5d6d8f9f24cabd48a
Anil Jangra <anil.jangra@idc.bedbath.com>
2021-04-22 12:46:11 +0530

    working commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Anil Jangra on 4/22/2021
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => <h1> App From Scratch</h1>
+ReactDOM.render(<App/>, document.getElementById('root'))
